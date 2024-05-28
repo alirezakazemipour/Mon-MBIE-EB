3e4464f2831d0bc2b1597e2742005ed58f7e2ae3
alirezakazemipour <kazemipour.alireza@gmail.com>
2024-05-28 13:14:42 -0600

    to save the code

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def run(cfg: DictConfig) -> None:
     experiment = MonExperiment(env, env_test, actor, critic, **cfg.experiment)
 
     return_train_history, return_test_history = experiment.train()
-    experiment.test()
+    # experiment.test()
 
     if cfg.experiment.debugdir is not None:
         from plot_gridworld_agent import plot_agent
@@ -89,46 +89,42 @@ def run(cfg: DictConfig) -> None:
 
 
 if __name__ == "__main__":
-    run()
+    # run()
     # algos = ["OFU_Solvable_NoPenalty", "OFU_Unsolvable_Cautious", "OFU_Solvable_Penalty"]
-    # for algo in algos:
-    #     runs = []
-    #     for i in range(100):
-    #         x = np.load(
-    #             f"data/{algo}/iStatelessBinaryMonitor/OFU/reward_model_test_{i}.npy")
-    #         runs.append(x)
-    #     smoothed = []
-    #     for run in runs:
-    #         val = [run[0] if not np.isnan(run[0]) else np.nanmin(run)]
-    #         for tmp in run[1:]:
-    #             tmp = tmp if not np.isnan(tmp) else np.nanmin(run[1:])
-    #             val.append(0.9 * val[-1] + 0.1 * tmp)
-    #         smoothed.append(val)
-    #     mean_return = np.mean(np.asarray(smoothed), axis=0)
-    #     std_return = np.std(np.asarray(smoothed), axis=0)
-    #     lower_bound = mean_return - 1.96 * std_return / math.sqrt(100)
-    #     upper_bound = mean_return + 1.96 * std_return / math.sqrt(100)
-    #     plt.fill_between(np.arange(4000),
-    #                      lower_bound,
-    #                      upper_bound,
-    #                      alpha=0.25
-    #                      )
-    #     plt.plot(np.arange(4000),
-    #              mean_return,
-    #              alpha=1,
-    #              label=algo,
-    #              linewidth=3
-    #              )
-    # plt.axhline(0.951, linestyle='--', label="optimal-Penalty", c="magenta")
-    # plt.axhline(0.99, linestyle='--', label="optimal-NoPenalty", c="olive")
-    # plt.xlabel("every 10 training steps")
-    # plt.ylabel("discounted (test?) return")
-    # plt.title(f" performance over {100} runs")
-    # plt.grid()
-    # plt.legend()
-    # plt.ylim([0.925, 0.96])
-    # plt.xlim([425, 625])
-    # # plt.ylim([-0.1, 1.1])
-    #
-    # plt.show()
-    #
+    algos = ["river_swim"]
+    for algo in algos:
+        runs = []
+        for i in range(20):
+            x = np.load(
+                f"data/{algo}/iButtonMonitor/OFU/reward_model_train_{i}.npy")
+            runs.append(x)
+        smoothed = []
+        for run in runs:
+            val = [run[0] if not np.isnan(run[0]) else np.nanmin(run)]
+            for tmp in run[1:]:
+                tmp = tmp if not np.isnan(tmp) else np.nanmin(run[1:])
+                val.append(0.9 * val[-1] + 0.1 * tmp)
+            smoothed.append(val)
+        mean_return = np.mean(np.asarray(smoothed), axis=0)
+        std_return = np.std(np.asarray(smoothed), axis=0)
+        lower_bound = mean_return - std_return
+        upper_bound = mean_return + std_return
+        plt.fill_between(np.arange(10000),
+                         lower_bound,
+                         upper_bound,
+                         alpha=0.25
+                         )
+        plt.plot(np.arange(10000),
+                 mean_return,
+                 alpha=1,
+                 label=algo,
+                 linewidth=3
+                 )
+    plt.axhline(21, linestyle='--', label="optimal", c="magenta")
+    plt.xlabel("every 10 training steps")
+    plt.ylabel("discounted (test?) return")
+    plt.title(f" performance over {100} runs")
+    plt.grid()
+    plt.legend()
+    plt.show()
+
