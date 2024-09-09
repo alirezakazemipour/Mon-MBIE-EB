08421e9f2a3506220bfd3c182085caf119306c98
alirezakazemipour <kazemipour.alireza@gmail.com>
2024-09-09 06:40:59 -0600

    some changes

--- submit.sh
+++ submit.sh
@@ -10,13 +10,14 @@
 #SBATCH --mem=6000M
 #SBATCH --cpus-per-task=32
 #SBATCH --tasks-per-node=1
-#SBATCH --job-name="running_everything"
+#SBATCH --job-name="level_unknown_beluga"
 # ---------------------------------------------------------------------
 echo "Current working directory: `pwd`"
 echo "Starting run at: `date`"
 # ---------------------------------------------------------------------
 # Run your simulation step here..
 
+job="level_unknown_beluga"
 module load python
 cp -r ofu $SLURM_TMPDIR/
 cp venv.tar.gz $SLURM_TMPDIR/
@@ -25,8 +26,8 @@ tar -xf venv.tar.gz
 source venv/bin/activate
 cd ofu/ || exit
 python main.py -m hydra/launcher=joblib hydra/sweeper=manual_sweeper >/dev/null
-tar -cavf data.tar.xz data
-cp data.tar.xz ~/projects/def-mtaylor3/alirezak
+tar -cavf data_$job.tar.xz data
+cp data_$job.tar.xz ~/projects/def-mtaylor3/alirezak
 
 
 
