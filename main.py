7538e087530f79480454004729e4720cd9674afd
alirezakazemipour <kazemipour.alireza@gmail.com>
2024-09-13 09:15:22 -0600

    by reducing beta, 1% is also solvable

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ def run(cfg: DictConfig) -> None:
     data = experiment.train()
     print(f"\ntotal episodes: {experiment.tot_episodes}")
     print(f"\nexplore episodes: {experiment.explore_episodes}")
-    # print("\nvisits:", critic.env_visit.astype(int))
-    # print("\nobservs:", critic.env_obsrv_count.astype(int))  # noqa
+    print("\nvisits:", critic.env_visit.astype(int))
+    print("\nobservs:", critic.env_obsrv_count.astype(int))  # noqa
     print("\nrwd model:", critic.env_rwd_model)
     # print("\njoint count: ", critic.joint_count[-1])
     # print("\nmon rwd: ", critic.mon_rwd_model)
