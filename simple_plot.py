e58d3c89e65c28fc11bfad0c4bdcdef7dcbd7550
alirezakazemipour <kazemipour.alireza@gmail.com>
2024-09-28 22:00:19 -0600

    add plotting changes

--- simple_plot.py
+++ simple_plot.py
@@ -19,40 +19,46 @@ plt.rc('figure', titlesize=BIGGER_SIZE)
 plt.title(f"EOP", weight="bold")
 
 n_runs = 30
-monitor = "Ask", #"Ask",
+monitor = "Ask",  "Level", "N", "Button"
 env = (
-    # "RiverSwim-6-v0",
-    # "Gridworld-Penalty-3x3-v0",
-    # "Gridworld-Corridor-3x4-v0",
-    # "Gridworld-Empty-Distract-6x6-v0",
+    "RiverSwim-6-v0",
+    "Gridworld-Penalty-3x3-v0",
+    "Gridworld-Corridor-3x4-v0",
+    "Gridworld-Empty-Distract-6x6-v0",
     "Gridworld-TwoRoom-Quicksand-3x5-v0",
-    # "Gridworld-Quicksand-Distract-4x4-v0",
+    "Gridworld-Quicksand-Distract-4x4-v0",
 )
 env_mon_combo = itertools.product(env, monitor)
 
 info = {"RiverSwim-6-v0": {"Ask": (199.14, "optimal"),
                            "Button": (192.72, "optimal"),
                            "Level": (199.14, "optimal"),
+                           "N": (199.14, "optimal"),
                            },
         "Gridworld-Penalty-3x3-v0": {"Ask": (9.415, "cautious"),
                                      "Button": (8.878, "cautious"),
                                      "Level": (9.415, "cautious"),
+                                     "N": (9.415, "cautious"),
                                      },
         "Gridworld-Corridor-3x4-v0": {"Ask": (9.409, "optimal"),
                                       "Button": (8.972, "optimal"),
                                       "Level": (9.409, "optimal"),
+                                      "N": (9.409, "optimal"),
                                       },
         "Gridworld-Empty-Distract-6x6-v0": {"Ask": (9.044, "cautious"),
                                             "Button": (8.057, "cautious"),
-                                            "Level": (9.044, "optimal")
+                                            "Level": (9.044, "cautious"),
+                                            "N": (9.044, "cautious"),
                                             },
         "Gridworld-TwoRoom-Quicksand-3x5-v0": {"Ask": (9.044, "cautious"),
                                                "Button": (8.413, "cautious"),
                                                "Level": (9.044, "cautious"),
+                                               "N": (9.044, "cautious"),
                                                },
         "Gridworld-Quicksand-Distract-4x4-v0": {"Ask": (9.135, "optimal"),
                                                 "Button": (8.483, "optimal"),
                                                 "Level": (9.135, "optimal"),
+                                                "N": (9.135, "optimal"),
                                                 },
         }
 
@@ -61,12 +67,12 @@ for env, monitor in env_mon_combo:
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     algos = [
-        # (f"{monitor}_1.0", "blue", "100%"),
+        (f"{monitor}_1.0", "blue", "100%"),
         # (f"{monitor}_0.75", "red", "75%"),
         # (f"{monitor}_0.5", "green", "50%"),
         # (f"{monitor}_0.25", "orange", "25%"),
         # (f"{monitor}_0.1", "brown", "10%"),
-        (f"{monitor}_0.01", "magenta", "1%")
+        # (f"{monitor}_0.01", "magenta", "1%")
     ]
 
     assert n_runs == 30
@@ -76,9 +82,9 @@ for env, monitor in env_mon_combo:
         ref, opt_caut = info[env][monitor]
         runs = []
         for i in range(n_runs):
-            x = np.load(f"data/Gym-Grid/{env}/{algo}/data_5.npz")["test_return"]
+            x = np.load(f"data/Gym-Grid/{env}/{algo}/data_{i}.npz")["test_return"]
             runs.append(x)
-        # print(np.argmax(np.array(runs).sum(-1)))
+        # print(np.argmin(np.array(runs).sum(-1)))
         # exit()
         smoothed = []
         for run in runs:
@@ -114,8 +120,9 @@ for env, monitor in env_mon_combo:
     plt.xlabel("training steps (x100)", weight="bold", fontsize=18)
     ax.set_xticks(np.arange(0, len(mean_return) + 0.1, 50))
 
-    plt.show()
-    # plt.savefig(f"/Users/alirezakazemipour/Desktop/{monitor}_{env}.pdf",
-    #             format="pdf",
-    #             bbox_inches="tight"
-    #             )
+    # plt.show()
+    plt.savefig(f"/Users/alirezakazemipour/Desktop/{monitor}_{env}.pdf",
+                format="pdf",
+                bbox_inches="tight"
+                )
+    plt.close()
