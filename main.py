1ad33a0a4a6eb218759ea449b7c6fb0b36d5764e
alirezakazemipour <kazemipour.alireza@gmail.com>
2024-10-21 08:34:14 -0600

    make the code ready to test

--- main.py
+++ main.py
@@ -42,13 +42,13 @@ def run(cfg: DictConfig) -> None:
                                )
     data = experiment.train()
     # experiment.test()
-    print(f"\ntotal episodes: {experiment.tot_episodes}")
-    print(f"\nexplore episodes: {experiment.explore_episodes}")
-    print("\nvisits:", critic.env_visit.astype(int))
-    print("\nobservs:", critic.env_obsrv_count.astype(int))  # noqa
-    print("\nrwd model:", critic.env_rwd_model)
+    # print(f"\ntotal episodes: {experiment.tot_episodes}")
+    # print(f"\nexplore episodes: {experiment.explore_episodes}")
+    # print("\nvisits:", critic.env_visit.astype(int))
+    # print("\nobservs:", critic.env_obsrv_count.astype(int))  # noqa
+    # print("\nrwd model:", critic.env_rwd_model)
     # print("\njoint count: ", critic.joint_count[-1])
-    print("\nmon rwd: ", critic.mon_rwd_model)
+    # print("\nmon rwd: ", critic.mon_rwd_model)
     # print("\ndynamics: ", critic.joint_dynamics)
     # print(critic.joint_q)
 
