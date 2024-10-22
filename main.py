e29e6e22699c04f765977da92cfcd6cc34d1058c
alirezakazemipour <kazemipour.alireza@gmail.com>
2024-10-22 14:47:40 -0600

    fix the max q

--- main.py
+++ main.py
@@ -42,20 +42,20 @@ def run(cfg: DictConfig) -> None:
                                )
     data = experiment.train()
     # experiment.test()
-    print(f"\ntotal episodes: {experiment.tot_episodes}")
-    print(f"\nexplore episodes: {experiment.explore_episodes}")
-    print("\nvisits:", critic.env_visit.astype(int))
-    print("\nobservs:", critic.env_obsrv_count.astype(int))  # noqa
-    print("\nrwd model:", critic.env_rwd_model)
+    # print(f"\ntotal episodes: {experiment.tot_episodes}")
+    # print(f"\nexplore episodes: {experiment.explore_episodes}")
+    # print("\nvisits:", critic.env_visit.astype(int))
+    # print("\nobservs:", critic.env_obsrv_count.astype(int))  # noqa
+    # print("\nrwd model:", critic.env_rwd_model)
     # print("\njoint count: ", critic.joint_count[-1])
     # print("\nmon rwd: ", critic.mon_rwd_model)
     # print("\ndynamics: ", critic.joint_dynamics)
-    print(critic.joint_q)
+    # print(critic.joint_q)
 
     if cfg.experiment.datadir is not None:
         filepath = os.path.join(cfg.experiment.datadir,
                                 cfg.environment.id,
-                                cfg.monitor.id + "_" + str(cfg.monitor.prob)
+                                cfg.monitor.id
                                 )
         os.makedirs(filepath, exist_ok=True)
         seed = str(cfg.experiment.rng_seed)
