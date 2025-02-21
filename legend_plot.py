feabe34e46cf09b1dbd66bfc66a2dd89096b3b54
alirezakazemipour <kazemipour.alireza@gmail.com>
2025-02-21 13:45:25 -0700

    update the legends

--- legend_plot.py
+++ legend_plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 plt.yticks([])
 plt.xticks([])
-plt.rcParams["font.family"] = 'serif'
+plt.rcParams["font.family"] = 'sans-serif'
 
 my_color = "#2a9d8f"
 simone_color = "#f4a261"
@@ -14,16 +14,16 @@ colors = [my_color, simone_color, known_color, "black"]
 f = lambda m, c: plt.plot([], [], marker=m, color=c, linewidth=4, linestyle='--')[0]
 handles = [f("_", colors[i]) for i in range(3)]
 # handles.append(f("_", colors[-1]))
-labels = ["Monitored MBIE-EB", "Directed-E$\mathbf{^2}$", "Known Monitor", "Minimax-Optimal"]
+labels = ["Monitored MBIE-EB", "Directed-E$^2$", "Known Monitor", "Minimax-Optimal"]
 handles.append(plt.axhline(5, linestyle="dashed", color="black", linewidth=3, label="Optimal"))
-legend = plt.legend(handles, labels, loc=3, framealpha=1, frameon=True, ncols=4, prop={'weight': 'bold'})
+legend = plt.legend(handles, labels, loc=(1, 1), framealpha=1, frameon=False, ncols=4)
 
 
 def export_legend(legend, filename="legend.png"):
     fig = legend.figure
     fig.canvas.draw()
     bbox = legend.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-    plt.savefig(f"/Users/alirezakazemipour/Desktop/legend.pdf",
+    plt.savefig(f"/Users/alirezakazemipour/Desktop/all_legend.pdf",
                 format="pdf",
                 bbox_inches=bbox
                 )
