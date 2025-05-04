b8828fd8920d6114bb05c9dd1e07c2e4aa8d297e
alirezakazemipour <kazemipour.alireza@gmail.com>
2025-05-04 14:41:28 -0600

    to update

--- legend_plot.py
+++ legend_plot.py
@@ -12,13 +12,13 @@ known_color = "#C26DBC"
 mbie_color = "#bc4749"
 pessimism_color = "#7b2cbf"
 
-colors = [my_color, mbie_color, "black"]
+colors = [my_color, pessimism_color, "black"]
 f = lambda m, c: plt.plot([], [], marker=m, color=c, linewidth=4, linestyle='--')[0]
 handles = [f("_", colors[i]) for i in range(2)]
 # handles.append(f("_", colors[-1]))
-labels = ["Monitored MBIE-EB", "Optimistic MBIE-EB", "Minimax-Optimal"]
+labels = ["Monitored MBIE-EB", "Pessimistic MBIE-EB", "Minimax-Optimal"]
 handles.append(plt.axhline(5, linestyle="dashed", color="black", linewidth=3, label="Optimal"))
-legend = plt.legend(handles, labels, loc=3, framealpha=1, frameon=True, ncols=3)
+legend = plt.legend(handles, labels, loc=(1, 1), framealpha=1, frameon=False, ncols=3)
 
 
 def export_legend(legend, filename="legend.png"):
@@ -32,4 +32,4 @@ def export_legend(legend, filename="legend.png"):
 
 
 export_legend(legend)
-# plt.show()
+plt.show()
