048861f0c8afcdc4229c157a24913c3d882a7fe4
alirezakazemipour <kazemipour.alireza@gmail.com>
2025-05-25 12:49:44 -0600

    adjust the readme and the simple plot

--- simple_plot.py
+++ simple_plot.py
@@ -4,6 +4,17 @@ import numpy as np
 import itertools
 from matplotlib import ticker
 import os
+import warnings
+
+de2_exists = True
+if not os.path.exists("data/DE2"):
+    de2_exists = False
+    warnings.warn("Directed-E$^2$ data not found. Ignoring it.")
+    
+mon_mbie_eb_exists = True
+if not os.path.exists("data/Mon_MBIE_EB"):
+    mon_mbie_eb_exists = False
+    warnings.warn("Mon_MBIE_EB data not found. Ignoring it.")
 
 mon_mbie_eb_color = "#2a9d8f"  # noqa
 de2_color = "#f4a261"
@@ -37,31 +48,35 @@ for env, monitor, prob in env_mon_p_combo:
 
     ref, ref_label = info[env][monitor]
     mon_mbie_eb_runs = []
-    dee_runs = []
+    de2_runs = []
     knm_runs = []
     for i in range(n_runs):
-        x = np.load(f"data/Mon_MBIE_EB/{env}/{monitor}_{prob}/data_{i}.npz")["test_return"]
-        mon_mbie_eb_runs.append(x)
-        x = np.load(f"data/DEE/{env}/{monitor}_{prob}/data_{i}.npz")["test/return"]
-        dee_runs.append(x)
+        if mon_mbie_eb_exists:
+            x = np.load(f"data/Mon_MBIE_EB/{env}/{monitor}_{prob}/data_{i}.npz")["test_return"]
+            mon_mbie_eb_runs.append(x)
+        if de2_exists:
+            x = np.load(f"data/DE2/{env}/{monitor}_{prob}/data_{i}.npz")["test/return"]
+            de2_runs.append(x)
         x = np.load(f"data/Known_Monitor/{env}/{monitor}_{prob}/data_{i}.npz")["test_return"]
         knm_runs.append(x)
 
     mon_mbie_eb_smoothed = []
-    dee_smoothed = []
+    de2_smoothed = []
     knm_smoothed = []
 
-    for run in mon_mbie_eb_runs:
-        val = [run[0]]
-        for tmp in run[1:]:
-            val.append(0.9 * val[-1] + 0.1 * tmp)
-        mon_mbie_eb_smoothed.append(val)
+    if mon_mbie_eb_exists:
+        for run in mon_mbie_eb_runs:
+            val = [run[0]]
+            for tmp in run[1:]:
+                val.append(0.9 * val[-1] + 0.1 * tmp)
+            mon_mbie_eb_smoothed.append(val)
 
-    for run in dee_runs:
-        val = [run[0]]
-        for tmp in run[1:]:
-            val.append(0.9 * val[-1] + 0.1 * tmp)
-        dee_smoothed.append(val)
+    if de2_exists:
+        for run in de2_runs:
+            val = [run[0]]
+            for tmp in run[1:]:
+                val.append(0.9 * val[-1] + 0.1 * tmp)
+            de2_smoothed.append(val)
 
     for run in knm_runs:
         val = [run[0]]
@@ -69,41 +84,43 @@ for env, monitor, prob in env_mon_p_combo:
             val.append(0.9 * val[-1] + 0.1 * tmp)
         knm_smoothed.append(val)
 
-    mon_mbie_eb_mean_return = np.mean(np.asarray(mon_mbie_eb_smoothed), axis=0)
-    mon_mbie_eb_std_return = np.std(np.asarray(mon_mbie_eb_smoothed), axis=0)
-    mon_mbie_eb_lower_bound = mon_mbie_eb_mean_return - 1.96 * mon_mbie_eb_std_return / math.sqrt(n_runs)
-    mon_mbie_eb_upper_bound = mon_mbie_eb_mean_return + 1.96 * mon_mbie_eb_std_return / math.sqrt(n_runs)
-    ax.fill_between(np.arange(len(mon_mbie_eb_mean_return)),
-                    mon_mbie_eb_lower_bound,
-                    mon_mbie_eb_upper_bound,
-                    alpha=0.25,
-                    color=mon_mbie_eb_color
-                    )
-    ax.plot(np.arange(len(mon_mbie_eb_mean_return)),
-            mon_mbie_eb_mean_return,
-            alpha=1,
-            linewidth=4,
-            c=mon_mbie_eb_color,
-            label="Mon-MBIE-EB"
-            )
+    if mon_mbie_eb_exists:
+        mon_mbie_eb_mean_return = np.mean(np.asarray(mon_mbie_eb_smoothed), axis=0)
+        mon_mbie_eb_std_return = np.std(np.asarray(mon_mbie_eb_smoothed), axis=0)
+        mon_mbie_eb_lower_bound = mon_mbie_eb_mean_return - 1.96 * mon_mbie_eb_std_return / math.sqrt(n_runs)
+        mon_mbie_eb_upper_bound = mon_mbie_eb_mean_return + 1.96 * mon_mbie_eb_std_return / math.sqrt(n_runs)
+        ax.fill_between(np.arange(len(mon_mbie_eb_mean_return)),
+                        mon_mbie_eb_lower_bound,
+                        mon_mbie_eb_upper_bound,
+                        alpha=0.25,
+                        color=mon_mbie_eb_color
+                        )
+        ax.plot(np.arange(len(mon_mbie_eb_mean_return)),
+                mon_mbie_eb_mean_return,
+                alpha=1,
+                linewidth=4,
+                c=mon_mbie_eb_color,
+                label="Mon-MBIE-EB"
+                )
 
-    dee_mean_return = np.mean(np.asarray(dee_smoothed), axis=0)
-    dee_std_return = np.std(np.asarray(dee_smoothed), axis=0)
-    dee_lower_bound = dee_mean_return - 1.96 * dee_std_return / math.sqrt(n_runs)
-    dee_upper_bound = dee_mean_return + 1.96 * dee_std_return / math.sqrt(n_runs)
-    ax.fill_between(np.arange(len(dee_mean_return)),
-                    dee_lower_bound,
-                    dee_upper_bound,
-                    alpha=0.25,
-                    color=de2_color
-                    )
-    ax.plot(np.arange(len(dee_mean_return)),
-            dee_mean_return,
-            alpha=1,
-            linewidth=4,
-            c=de2_color,
-            label="Directed-E$^2$"
-            )
+    if de2_exists:
+        de2_mean_return = np.mean(np.asarray(de2_smoothed), axis=0)
+        de2_std_return = np.std(np.asarray(de2_smoothed), axis=0)
+        de2_lower_bound = de2_mean_return - 1.96 * de2_std_return / math.sqrt(n_runs)
+        de2_upper_bound = de2_mean_return + 1.96 * de2_std_return / math.sqrt(n_runs)
+        ax.fill_between(np.arange(len(de2_mean_return)),
+                        de2_lower_bound,
+                        de2_upper_bound,
+                        alpha=0.25,
+                        color=de2_color
+                        )
+        ax.plot(np.arange(len(de2_mean_return)),
+                de2_mean_return,
+                alpha=1,
+                linewidth=4,
+                c=de2_color,
+                label="Directed-E$^2$"
+                )
 
     knm_mean_return = np.mean(np.asarray(knm_smoothed), axis=0)
     knm_std_return = np.std(np.asarray(knm_smoothed), axis=0)
